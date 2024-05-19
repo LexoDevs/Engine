9a055c94951f90c9789263a5a529d11e617bb1cc
LexoDevs <alexdepy8@gmail.com>
2024-05-19 15:34:19 +0200

    Main Project
    Engine Testing

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+﻿
+#include "app.hpp"
+
+// std
+#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
+
+int main() {
+
+    std::cout << "Cargando shaders:";
+
+    system("C:/dev/Lexon/Lexon/shaders/compile.bat");
+
+    lve::FirstApp app{};
+
+    try {
+        app.run();
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << '\n';
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
